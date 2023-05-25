c910ef00f9db2e134aa2bf2f348956310c6d63e8
SabahaddinIsp <126579800+SabahaddinIsp@users.noreply.github.com>
2023-05-25 10:48:22 +0300

    2

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
+import tkinter as tk
+from tkinter import ttk
 from bs4 import BeautifulSoup
 import requests
+import re
 
-
-url = f"https://weather.com/tr-TR/weather/tenday/l/İzmir+İzmir?canonicalCityId=a3722d3ba43ddbef656021ba77ee61bf4c6fae20636732a1f2958d22beb70107"
+url = f"https://www.mynet.com/hava-durumu/izmir-haftalik-hava-durumu"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
-page_text = doc.find(class_="DailyContent--narrative--3Ti6_")
-data = str(page_text).split("En ")[-1].split(".")[0].split()[-1]
-print(data)
+def today():
+    page_text1 = doc.find(class_="weather-list-box mx-3")
+
+    nem1 = str(page_text1).split("Nem:")[1].split("</div>")[0]
+    print(nem1)
+    temps1 = str(page_text1).split("düşük:")[1].split("</div>")[0]
+    print(temps1)
+    yagis1 = str(page_text1).split("İhtimali: ")[1].split("</div>")[0]
+    print(yagis1)
+    rüzgar1 = str(page_text1).split("Rüzgar Hızı:")[1].split(" </div>")[0]
+    print(rüzgar1)
+
+
+def secondDay():
+    page_text = doc.find(class_="d-flex weather-list-item-container")
+
+    nem = str(page_text).split("Nem:</strong>")[1].split("</div>")[0]
+    print(nem)
+    temps = str(page_text).split("düşük:</strong>")[1].split("</div>")[0]
+    print(temps)
+    yagis = str(page_text).split("Yağış İhtimali:</strong>")[1].split(" </div>")[0]
+    print(yagis)
+    rüzgar = str(page_text).split("Rüzgar Hızı:</strong>")[1].split(" </div>")[0]
+    print(rüzgar)
+
+def thirdDay():
+    page_text = doc.findAll(class_="d-flex weather-list-item-container")
+    day2= str(page_text).split("<strong>Hissedilen:</strong>")[2]
+
+    nem = str(day2).split("Nem:</strong>")[1].split("</div>")[0]
+    print(nem)
+    temps = str(day2).split("düşük:</strong>")[1].split("</div>")[0]
+    print(temps)
+    yagis = str(day2).split("Yağış İhtimali:</strong>")[1].split(" </div>")[0]
+    print(yagis)
+    rüzgar = str(day2).split("Rüzgar Hızı:</strong>")[1].split(" </div>")[0]
+    print(rüzgar)
+
+today()
+secondDay()
+thirdDay()
+
