2c841cb662d4d883fc48c47683569c6491783eb5
NipiptheFae <127285929+NipiptheFae@users.noreply.github.com>
2023-05-25 19:01:33 +0300

    added gui

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from bs4 import BeautifulSoup
+from tkinter import *
 import requests
 import re
 
 url = f"https://www.mynet.com/hava-durumu/izmir-haftalik-hava-durumu"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
+
 
 def today():
     page_text1 = doc.find(class_="weather-list-box mx-3")
@@ -49,4 +51,41 @@ def thirdDay():
 today()
 secondDay()
 thirdDay()
+
+
+###################       GUI
+root = Tk()
+root.title("Weather Information Display Program")
+title = tk.Label(root, text='Welcome!')
+title.pack()
+title2 = tk.Label(root, text='Please choose your city.')
+title2.pack()
+
+def show():
+        myLabel = Label(root,text=clicked.get()).pack()
+
+clicked = StringVar()
+clicked.set("Ankara")
+
+####        DROPDOWN LIST
+drop = OptionMenu(root, clicked, "Ankara","Istanbul","Izmir","Bursa","Antalya","Konya","Adana","Sanliurfa","Gaziantep","Kocaeli")
+
+confirmButton = Button(root,text="Confirm") #add command here!
+drop.pack()
+
+####        OPTION LIST
+myButton = Button(root,text="Today", command=today).pack()
+myButton2 = Button(root,text="Tomorrow", command=secondDay).pack()
+myButton3 = Button(root,text="2 Days Later", command=thirdDay).pack()
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
 
